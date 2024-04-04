ddf9a886bf88c6aa79fb5f8913a52070a813e0d6
Chelsea Alambuya <64859402+alambuyachelsea@users.noreply.github.com>
2024-04-04 11:27:39 +0200

    exercise3 ???

--- Exercise3.py
+++ Exercise3.py
@@ -12,10 +12,32 @@ image_index = 209
 image = np.array(train_images[image_index], dtype='uint8')
 image = image.reshape((28, 28))
 
+
 plt.imshow(image, cmap='gray')
 plt.axis('off')
 plt.show()
 
 print(train_labels[image_index])
 print(mndata.display(train_images[image_index]))
-push
+
+# Get the coordinates of points
+x_coords, y_coords = np.where(image > 0)
+
+# Plot the points
+plt.scatter(x_coords, 28 - y_coords, color='black', s=5)
+# Invert y-axis to match image orientation
+
+plt.gca().set_aspect('equal', adjustable='box')
+# Set aspect ratio to maintain square pixels
+
+plt.gca().invert_yaxis()  # Invert y-axis to match image orientation
+plt.axis('off')
+plt.show()
+
+"""
+    crying sobbing, why did i choose this life for myself
+    Idea to solve
+    Use 2 features to create an expection for each figure
+    1. Intensity distribution
+    2. ?? more info needed
+"""
